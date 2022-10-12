94b97cd8d3970fc8bd7d2eda3c074ccf7ee01659
Jordan Harband <ljharb@gmail.com>
2022-10-12 09:50:15 -0700

    [eslint] fix indentation and whitespace

--- example/eq.js
+++ example/eq.js
@@ -3,14 +3,14 @@
 var bufferEqual = require('../');
 
 console.dir(bufferEqual(
-    new Buffer([253,254,255]),
-    new Buffer([253,254,255])
+	new Buffer([253, 254, 255]),
+	new Buffer([253, 254, 255])
 ));
 console.dir(bufferEqual(
-    new Buffer('abc'),
-    new Buffer('abcd')
+	new Buffer('abc'),
+	new Buffer('abcd')
 ));
 console.dir(bufferEqual(
-    new Buffer('abc'),
-    'abc'
+	new Buffer('abc'),
+	'abc'
 ));
